517f5929408c4a5e5b58618c4e9e455824661e60
sateeshbadsha <110009052+sateeshbadsha@users.noreply.github.com>
2022-08-15 16:49:48 +0530

    added some exta message

--- devops.java
+++ devops.java
@@ -3,4 +3,4 @@ my name is k. sateesh kumar reddy
 i am new to linux
 i wnat learn linux beause i dont have any idear about linux
 linux is most importent for dwvops engineers
-
+linux very easy to lurn
